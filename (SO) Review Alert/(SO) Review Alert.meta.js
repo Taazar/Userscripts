27a30a52e7dccfdc20aa4d2c1e9f921a1871c783
Taazar <benrob99@hotmail.co.uk>
2019-12-17 16:00:19 +0000

    0.4 - Add auto update

--- (SO) Review Alert/(SO) Review Alert.meta.js
+++ (SO) Review Alert/(SO) Review Alert.meta.js
@@ -1,14 +1,3 @@
 // ==UserScript==
-// @name         (SO) Review Alert
-// @author       Taazar
-// @version      0.3
-// @description  Refresh review page until there are posts to review
-// @namespace    https://github.com/Taazar
-// @homepage     https://gist.github.com/Taazar/c88709ef84870a220bc06d73e7bb806e
-// @updateURL    https://gist.githubusercontent.com/Taazar/c88709ef84870a220bc06d73e7bb806e/raw/ba93c8a4a362481a9cbb5d16c2a92ad7a4cb75ee/Review%2520Alert%2520(Stackoverflow).meta.js
-// @downloadURL  https://gist.githubusercontent.com/Taazar/c88709ef84870a220bc06d73e7bb806e/raw/ba93c8a4a362481a9cbb5d16c2a92ad7a4cb75ee/Review%2520Alert%2520(Stackoverflow).js
-// @match        https://stackoverflow.com/review/*
-// @exclude      https://stackoverflow.com/review/*/stats*
-// @exclude      https://stackoverflow.com/review/*/history*
-// @grant        none
+// @version      0.4
 // ==/UserScript==
