82e4440cb1b35d4771d14d3a619ffc16cc260a9b
Taazar <benrob99@hotmail.co.uk>
2020-02-03 01:01:59 +0000

    0.2 - Add auto activate
    Now automatically submits the key if it passes a regex validity test

--- (Steam) Agree Key Terms/(Steam) Agree Key Terms.user.js
+++ (Steam) Agree Key Terms/(Steam) Agree Key Terms.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         (Steam) Agree Key Terms
 // @author       Taazar
-// @version      0.1
+// @version      0.2
 // @description  Automatically agree to terms and conditions when activating keys
 // @namespace    https://github.com/Taazar
 // @homepage     https://github.com/Taazar/Userscripts/tree/master/(Steam)%20Agree%20Key%20Terms
@@ -14,4 +14,10 @@
 (function() {
     'use strict';
     document.getElementById("accept_ssa").click();
+    if(window.location.href.indexOf("https://store.steampowered.com/account/registerkey?key=") > -1) {
+        var steamKey = new URLSearchParams(window.location.search).get("key")
+        if (/[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}/.test(steamKey)) {
+            document.getElementById('register_btn').click()
+        }
+    }
 })();
