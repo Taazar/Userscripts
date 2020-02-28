32788aa76b08352909f11449c19b8bef4b25c076
Taazar <benrob99@hotmail.co.uk>
2020-02-28 10:27:48 +0000

    1.0 - Create Script
    Adds the text name under each button on the top bar respectively.
    (Edit to fix indentation)

--- (SO) Improved Top Bar/(SO) Improved Top Bar.user.js
+++ (SO) Improved Top Bar/(SO) Improved Top Bar.user.js
@@ -20,23 +20,23 @@
 (function() {
 	'use strict';
 	((d,o,c,s) => {
-	  c = d.createElement("style");
-	  c.textContent = '.-link{display:flex;flex-direction:column;justify-content:center;}.-menuitem-caption{font-size: 10px;}';
-	  d.head.appendChild(c);
-	  for (let n in o) {
-		s = d.querySelector(o[n]);
-		if (!s) continue;
-		c = d.createElement("span");
-		c.className = "-menuitem-caption";
-		c.textContent = n;
-		s.appendChild(c);
-	  }
+		c = d.createElement("style");
+		c.textContent = '.-link{display:flex;flex-direction:column;justify-content:center;}.-menuitem-caption{font-size: 10px;}';
+		d.head.appendChild(c);
+		for (let n in o) {
+			s = d.querySelector(o[n]);
+			if (!s) continue;
+			c = d.createElement("span");
+			c.className = "-menuitem-caption";
+			c.textContent = n;
+			s.appendChild(c);
+		}
 	})(document, {
-	  inbox: ".js-inbox-button",
-	  awards: ".js-achievements-button",
-	  review: ".js-review-button",
-	  help: ".js-help-button",
-	  network: ".js-site-switcher-button",
-	  hats: ".-link.icon-winterbash"
+		inbox: ".js-inbox-button",
+		awards: ".js-achievements-button",
+		review: ".js-review-button",
+		help: ".js-help-button",
+		network: ".js-site-switcher-button",
+		hats: ".-link.icon-winterbash"
 	})
 })();
