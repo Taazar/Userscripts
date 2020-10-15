44d07bd48bdf75f1aa9f1474d98b48d6575a675b
Taazar <benrob99@hotmail.co.uk>
2020-10-15 08:55:37 +0100

    1.1 - Add username redirection

--- (MSOut) Redirect Subreddit Links/(MSOut) Redirect Subreddit Links.user.js
+++ (MSOut) Redirect Subreddit Links/(MSOut) Redirect Subreddit Links.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         (MSOut) Redirect Subreddit Links
 // @author       Taazar
-// @version      1.0
+// @version      1.1
 // @description  Redirect any follow subreddit links to proper reddit URLS
 // @namespace    https://github.com/Taazar
 // @homepage     https://github.com/Taazar/Userscripts/tree/master/(MSOut)%20Redirect%20Subreddit%20Links
 // @updateURL    https://raw.githubusercontent.com/Taazar/Userscripts/master/(MSOut)%20Redirect%20Subreddit%20Links/(MSOut)%20Redirect%20Subreddit%20Links.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Taazar/Userscripts/master/(MSOut)%20Redirect%20Subreddit%20Links/(MSOut)%20Redirect%20Subreddit%20Links.user.js
 // @match        https://pcottle.github.io/r/*
+// @match        https://pcottle.github.io/u/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 //1.0 Redirect any follow subreddit links to proper reddit URLS
+//1.1 Add redirection for username links
 
 window.location.replace(window.location.href.replace("pcottle.github.io", "reddit.com"));
