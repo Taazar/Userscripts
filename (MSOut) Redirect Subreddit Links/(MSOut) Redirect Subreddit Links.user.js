2bd130f6338daa8d5f2d380548a07e987ffe7021
Taazar <benrob99@hotmail.co.uk>
2020-12-11 16:03:29 +0000

    1.2 - Enable on both http and https

--- (MSOut) Redirect Subreddit Links/(MSOut) Redirect Subreddit Links.user.js
+++ (MSOut) Redirect Subreddit Links/(MSOut) Redirect Subreddit Links.user.js
@@ -7,13 +7,14 @@
 // @homepage     https://github.com/Taazar/Userscripts/tree/master/(MSOut)%20Redirect%20Subreddit%20Links
 // @updateURL    https://raw.githubusercontent.com/Taazar/Userscripts/master/(MSOut)%20Redirect%20Subreddit%20Links/(MSOut)%20Redirect%20Subreddit%20Links.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Taazar/Userscripts/master/(MSOut)%20Redirect%20Subreddit%20Links/(MSOut)%20Redirect%20Subreddit%20Links.user.js
-// @match        https://pcottle.github.io/r/*
-// @match        https://pcottle.github.io/u/*
+// @match        *pcottle.github.io/r/*
+// @match        *pcottle.github.io/u/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 //1.0 Redirect any follow subreddit links to proper reddit URLS
 //1.1 Add redirection for username links
+//1.2 Enable on both http and https
 
 window.location.replace(window.location.href.replace("pcottle.github.io", "reddit.com"));
