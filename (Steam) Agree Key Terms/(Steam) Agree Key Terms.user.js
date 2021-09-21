c26c354c9ec77d84a1ac06c8ae686bd33d1239c2
Taazar <benrob99@hotmail.co.uk>
2021-09-21 16:47:23 +0100

    0.3 - Add icon

--- (Steam) Agree Key Terms/(Steam) Agree Key Terms.user.js
+++ (Steam) Agree Key Terms/(Steam) Agree Key Terms.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         (Steam) Agree Key Terms
 // @author       Taazar
-// @version      0.2
+// @version      0.3
 // @description  Automatically agree to terms and conditions when activating keys
 // @namespace    https://github.com/Taazar
 // @homepage     https://github.com/Taazar/Userscripts/tree/master/(Steam)%20Agree%20Key%20Terms
 // @updateURL    https://raw.githubusercontent.com/Taazar/Userscripts/master/(Steam)%20Agree%20Key%20Terms/(Steam)%20Agree%20Key%20Terms.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Taazar/Userscripts/master/(Steam)%20Agree%20Key%20Terms/(Steam)%20Agree%20Key%20Terms.user.js
 // @match        https://store.steampowered.com/account/registerkey*
+// @icon         https://www.google.com/s2/favicons?domain=steampowered.com
 // @grant        none
 // ==/UserScript==
 
