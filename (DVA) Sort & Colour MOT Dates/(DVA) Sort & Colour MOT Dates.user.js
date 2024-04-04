281541fb898b093bd066d82165d1788fc203fffa
Ben Robinson <benrob99@hotmail.co.uk>
2024-04-04 15:47:31 +0100

    add auto-refresh with boolean options, refactor some vars to const, redesign output, improve commenting

--- (DVA) Sort & Colour MOT Dates/(DVA) Sort & Colour MOT Dates.user.js
+++ (DVA) Sort & Colour MOT Dates/(DVA) Sort & Colour MOT Dates.user.js
@@ -14,29 +14,35 @@
 // ==/UserScript==
 
 'use strict';
-window.onload = function () {
-	var dropClick = document.getElementsByClassName("btn dropdown-toggle btn-default bs-placeholder")[0];
-	dropClick.onclick = function () {
-		var expiryText = document.getElementById("certExpiryNotice").getElementsByClassName("info-notice")[0].innerText;
-		var expiryDateDMY = /(\d{2})\/(\d{2})\/(\d{4})/.exec(expiryText);
-		var expiryDate = Date.parse(expiryDateDMY[2] + "-" + expiryDateDMY[1] + "-" + expiryDateDMY[3]);
+const reloadOnNone = true;
+const reloadOnInvalidDates = true;
 
-		var dropDowns = document.getElementById("bs-select-1").getElementsByClassName("text-muted");
+window.onload = function () {
+	//Get choose combo box
+	const dropClick = document.getElementsByClassName("btn dropdown-toggle btn-default bs-placeholder")[0];
+	dropClick.onclick = function () {
+		const expiryText = document.getElementById("certExpiryNotice").getElementsByClassName("info-notice")[0].innerText;
+		const expiryDateDMY = /(\d{2})\/(\d{2})\/(\d{4})/.exec(expiryText);
+		const expiryDate = Date.parse(expiryDateDMY[2] + "-" + expiryDateDMY[1] + "-" + expiryDateDMY[3]);
+
+		const dropDowns = document.getElementById("bs-select-1").getElementsByClassName("text-muted");
 		var results = "";
 		var resultArray = [];
+		var anyValidDates = false;
 
 		for (var i = 0; i < dropDowns.length; i++) {
 			var nextDate = dropDowns[i].innerText;
-			//Sort (collect)
+			//Pre-Sort (collect and add IDs to fields for later)
 			dropDowns[i].parentElement.parentElement.parentElement.id = "Sorter" + i;
 			resultArray.push([Date.parse(nextDate), i]);
 
 			//Colour code
-			if (Date.parse(nextDate) >= expiryDate) {
+			if (Date.parse(nextDate) >= expiryDate) { //After expiry date
 				dropDowns[i].innerHTML = '<span style="background-color:red;color:white">' + nextDate + '</span>';
-			} else if (Date.parse(dropDowns[i].innerText) < expiryDate) {
+			} else if (Date.parse(dropDowns[i].innerText) < expiryDate) { //Before or on expiry date
 				dropDowns[i].innerHTML = '<span style="background-color:green;color:white">' + nextDate + '</span>';
 				results = results + "\n" + dropDowns[i].parentElement.innerText + " : " + nextDate;
+				anyValidDates = true;
 			}
 		}
 		setTimeout(() => {
@@ -47,17 +53,31 @@ window.onload = function () {
 				}
 				return a[0] - b[0] || isNaN(a[0]) - isNaN(b[0]);
 			});
+
 			//remove any previous outputs
-			$(".sticky-footer").contents(".optgroup-1").remove();
+			$(".SortedList").remove();
+
 			//add new sorted output to bottom of page
-			var container = document.getElementsByClassName("dropdown-menu inner ")[0];
-			var freeSpace = document.getElementsByClassName("pb-3")[0];
-			for (var j = resultArray.length - 1; j >= 0; j--) {
-				var a = document.getElementById("Sorter" + resultArray[j][1]).cloneNode(true);
-				freeSpace.after(a);
+			var newDiv = document.createElement("div");
+			newDiv.id = "SortedList";
+			for (var i = 0; i < resultArray.length; i++) {
+				var liClone = document.getElementById("Sorter" + resultArray[i][1]).cloneNode(true);
+				newDiv.appendChild(liClone);
+			}
+			document.getElementsByClassName("pb-3")[0].after(newDiv);
+
+			//if all "None Available" then refresh page
+			if (
+				(reloadOnNone && newDiv.firstChild.getElementsByClassName("text-muted")[0].innerText === "None Available")
+				|| (reloadOnInvalidDates && !anyValidDates)
+			) {
+				setTimeout(() => {
+					location.reload();
+				}, 5000);
 			}
 		}, 500);
 	}
+	//Auto click combo box after 700ms
 	setTimeout(() => {
 		dropClick.click();
 	}, 700);
